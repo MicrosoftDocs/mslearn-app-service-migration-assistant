04cd28625f11e53dbeae9cf90e4401aea85ae789
Nick Walker <nickwalk@microsoft.com>
2020-06-08 06:46:40 -0700

    Update vm-setup.ps1

--- vm-setup.ps1
+++ vm-setup.ps1
@@ -12,6 +12,16 @@ $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/partsunlimitedwebsite.deploy.cmd", "C:\Exercise\partsunlimitedwebsite.deploy.cmd")
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/partsunlimitedwebsite.SetParameters.xml", "C:\Exercise\partsunlimitedwebsite.SetParameters.xml")
 $wc.DownloadFile("https://raw.githubusercontent.com/MicrosoftDocs/mslearn-app-service-migration-assistant/master/setup.sql", "C:\Exercise\setup.sql")
+
+Start-Process "msiexec.exe" -ArgumentList '/I C:\Exercise\WebDeploy_amd64_en-US.msi /qn /norestart ADDLOCAL=ALL' -Wait -NoNewWindow
+Start-Process "msiexec.exe" -ArgumentList '/I "C:\Exercise\AppServiceMigrationAssistant.1.0.3.msi" /qn /norestart ALLUSERS=1' -Wait -NoNewWindow
+
+$sql = get-content C:\Exercise\setup.sql
+$cn = new-object system.data.SqlClient.SQLConnection($connstr);
+$cmd = new-object system.data.sqlclient.sqlcommand($sql, $cn);
+$cn.Open();
+$cmd.ExecuteNonQuery();
+$cn.Close();
 }
 
 install-windowsfeature -name web-server,web-mgmt-console,web-mgmt-service,web-asp-net45,web-http-redirect,web-custom-logging,web-log-libraries,web-request-monitor,web-http-tracing,web-basic-auth,web-windows-auth,web-appinit
@@ -25,17 +35,6 @@ Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
 
 
 wait-job $downloadJob
-
-
-Start-Process "msiexec.exe" -ArgumentList '/I C:\Exercise\WebDeploy_amd64_en-US.msi /qn /norestart ADDLOCAL=ALL' -Wait -NoNewWindow
-Start-Process "msiexec.exe" -ArgumentList '/I "C:\Exercise\AppServiceMigrationAssistant.1.0.3.msi" /qn /norestart ALLUSERS=1' -Wait -NoNewWindow
-
-$sql = get-content C:\Exercise\setup.sql
-$cn = new-object system.data.SqlClient.SQLConnection($connstr);
-$cmd = new-object system.data.sqlclient.sqlcommand($sql, $cn);
-$cn.Open();
-$cmd.ExecuteNonQuery();
-$cn.Close();
 
 (Get-Content c:\Exercise\partsunlimitedwebsite.SetParameters.xml).replace('[connection_string]', $connstr) | Set-Content c:\Exercise\partsunlimitedwebsite.SetParameters.xml
 & C:\Exercise\partsunlimitedwebsite.deploy.cmd /Y
